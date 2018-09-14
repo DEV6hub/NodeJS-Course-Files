78c136150534b2339b4b5e302ecfa5b451008843
parthcdev6 <parthchokshi@ParthDev6-Macbook-Pro.local>
2018-09-14 17:26:22 -0400

    cleanup

--- Unit08-Rxjs-Observables/src/count_in_files.js
+++ Unit08-Rxjs-Observables/src/count_in_files.js
@@ -2,7 +2,7 @@
 Count number of vowels and consonants in given files.
  */
 import fs from 'fs';
-import { bindNodeCallback,from, of } from 'rxjs';
+import { bindNodeCallback,from } from 'rxjs';
 import { switchMap, last, mergeMap, scan } from 'rxjs/operators';
 
 const vowels = ['a', 'e', 'i', 'o', 'u'];
@@ -12,12 +12,12 @@ const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 
 const readDir$ = bindNodeCallback(fs.readdir);
 
 // Read file
-let readFiles = bindNodeCallback(fs.readFile);
+const readFiles = bindNodeCallback(fs.readFile);
 
 // Get file content
-let getFileContent = file => readFiles('./sample/'+file,{encoding: 'utf8'});
+const getFileContent = file => readFiles('./sample/'+file,{encoding: 'utf8'});
 
-let gather = (fileArray, character) => {
+const gather = (fileArray, character) => {
 	//gather vowels
 	if (vowels.indexOf(character) > -1) {
 		fileArray[0]+=character;
@@ -31,9 +31,9 @@ let gather = (fileArray, character) => {
 	return fileArray;
 };
 
-let count = (totalCount, current) => {
+const count = (totalCount, current) => {
 
-	let curr = current.toLowerCase();
+	const curr = current.toLowerCase();
 
 	if(vowels.indexOf(curr) > -1) {
 
@@ -65,7 +65,7 @@ let count = (totalCount, current) => {
 
 };
 // sort object in alphabetical order
-let sort = newFile => {
+const sort = newFile => {
 
 	let newVowelObject = {}, newConsonantObject = {};
 
@@ -89,7 +89,7 @@ let sort = newFile => {
 
 };
 
-let source = readDir$('./sample')
+const source = readDir$('./sample')
 	.pipe(
 		mergeMap(file => file),
 		mergeMap(file => getFileContent(file)),
